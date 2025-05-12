711635772d46e0c9fd152e2efd6e047b947fc1e1
Pete Berkhout <c.berkhout@tcrmbo.nl>
2025-05-12 09:34:29 +0200

    supplier functies start

--- sup-crud-get.php
+++ sup-crud-get.php
@@ -1,1 +1,65 @@
-<?php include "underconstruct.php"; ?>
+<!DOCTYPE html>
+<html lang="nl"> 
+<head>
+	 <meta charset="UTF-8">
+	 <title>Bread Company</title>
+	 <link rel="stylesheet" type="text/css" href="company.css">  
+</head>
+
+<body>
+	<header>
+		<h1>Welkom bij de Bread Company</h1>
+		<!-- hieronder wordt het menu opgehaald. -->
+		<?php
+			session_start(); 
+			include "nav.html";
+		?>
+	</header>
+ 	<?php
+	require_once "dbconnect.php";
+	try {
+		$sQuery = "SELECT * FROM supplier";
+		$oStmt = $db->prepare($sQuery);
+		$oStmt->execute();
+
+		echo "<h2 class='centering'>Onderhoud leveranciers</h2>";
+		if ($oStmt->rowCount() > 0) {
+			echo '<table class="tabledisp2">';
+			echo '<thead>';
+			echo '<td>Lev.nr.</td>';
+			echo '<td>Lev.naam</td>';
+			echo '<td>Adres</td>';
+			echo '<td>Huisnr</td>';
+			echo '<td>Woonplaats</td>';
+            echo '<td>Actieknop</td>';
+			echo '</thead>';
+			while ($aRow = $oStmt->fetch(PDO::FETCH_ASSOC)) {
+				echo '<tr><form action="sup-crud-upd.php" method="POST">';
+				echo '<td><input type="number" readonly name="sel-supp-pk" value="' . $aRow['ID'] . '"></td>';
+				echo '<td>' . $aRow['company'] . '</td>';
+				echo '<td>' . $aRow['adress'] . '</td>';
+				echo '<td>' . $aRow['streetnr'] . '</td>';
+				echo '<td>' . $aRow['city'] . '</td>';
+				echo '<td><input type="submit" value="Wijzig" name="submt-sel-supp-upd">.&nbsp;&nbsp;';
+                echo '<input type="submit" value="Verwijder" name="submt-sel-supp-del" formaction="sup-crud-del.php"></td>';
+				echo '</form></tr>';
+			}
+			echo '</table>';
+		} else {
+			echo 'Helaas, geen gegevens bekend';
+		}
+	} catch (PDOException $e) {
+		$sMsg = '<p> 
+					Regelnummer: ' . $e->getLine() . '<br /> 
+					Bestand: ' . $e->getFile() . '<br /> 
+					Foutmelding: ' . $e->getMessage() . ' 
+				</p>';
+
+		trigger_error($sMsg);
+	}
+	$db = null;
+	?>
+
+
+</body>
+</html>
